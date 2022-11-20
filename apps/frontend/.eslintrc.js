5b02e33890ba0ee6a83493708b084f915124ef2d
Ermakov <erm-dev@ya.ru>
2022-11-20 20:41:03 +0300

    feat: Добавить плагины

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -11,9 +11,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020
   },
+  plugins: ['simple-import-sort'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
   },
   overrides: [
     {
